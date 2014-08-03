bab302c6fa9727962a29fc959ed76d505a3f57b9
nolbu <nolbu@localhost.localdomain>
2014-08-03 10:00:55 +0900

    finished ep1 of turtorial

---
+++ src/com/nolbu/rest/status/V1_status.java
@@ -0,0 +1,24 @@
+package com.nolbu.rest.status;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("v1/status")
+public class V1_status {
+	private static final String api_version = "00.01.00";
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle(){
+		return "<p>Java Web Service</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion(){
+		return "<p>Version : " + api_version + "</p>";
+	}	
+}
