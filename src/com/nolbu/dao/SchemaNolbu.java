d2462a8bba71ac97d897750c33a6edfd8b3a36ff
nolbu <nolbu@localhost.localdomain>
2014-08-04 17:51:43 +0900

    parameter finsihed

--- src/com/nolbu/dao/SchemaNolbu.java
+++ src/com/nolbu/dao/SchemaNolbu.java
@@ -10,6 +10,7 @@ import org.codehaus.jettison.json.JSONArray;
 import com.nolbu.util.ToJSON;
 
 public class SchemaNolbu extends OracleNolbu {
+
     public JSONArray queryReturnBrandParts(String brand) throws Exception{
 	  	PreparedStatement query = null;
 	  	Connection conn = null;
@@ -23,6 +24,39 @@ public class SchemaNolbu extends OracleNolbu {
 	  		    										  " FROM PC_PARTS " + 
 	  		    											" WHERE UPPER(PC_PARTS_MAKER) = ? ");
 	  	    query.setString(1,  brand.toUpperCase());
+	  	    ResultSet rs = query.executeQuery();
+	  	    
+	  	    json = converter.toJSONArray(rs);
+	  	    query.close();
+	  	    	
+	  	} catch (SQLException sqlError){
+  	    	sqlError.printStackTrace();
+    		return json;
+	  	} catch (Exception e){
+	  	    e.printStackTrace();
+	  	    return json;
+	  	} finally{
+	  	    if(conn != null) conn.close();
+	  	}
+	  	
+	  	return json;
+           }
+
+    public JSONArray queryReturnBrandItemNumber(String brand, int item_number) throws Exception{
+	  	PreparedStatement query = null;
+	  	Connection conn = null;
+	  	
+	  	ToJSON converter = new ToJSON();
+	  	JSONArray json = new JSONArray();
+	  	
+	  	try{
+	  	    conn = oraclePcPartsConnection();
+	  	    query = conn.prepareStatement("SELECT PC_PARTS_PK,PC_PARTS_TITLE, PC_PARTS_CODE, PC_PARTS_MAKER, PC_PARTS_AVAIL,PC_PARTS_DESC"+ 
+	  		    										  " FROM PC_PARTS " + 
+	  		    											" WHERE UPPER(PC_PARTS_MAKER) = ? " +
+	  		    										                " AND PC_PARTS_CODE = ? ");
+	  	    query.setString(1,  brand.toUpperCase());
+	  	    query.setInt(2,  item_number);
 	  	    ResultSet rs = query.executeQuery();
 	  	    
 	  	    json = converter.toJSONArray(rs);
--- src/com/nolbu/rest/inventory/V2_Inventory.java
+++ src/com/nolbu/rest/inventory/V2_Inventory.java
@@ -47,6 +47,14 @@ public class V2_Inventory {
 		return Response.ok(returnString).build();
 	}    
 
+/*	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnErrorOnBrand() throws Exception{
+	    return Response.status(400).entity("Error: please specify brand for this search");
+	}
+*/
+	
 	@Path("/{brand}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -71,4 +79,28 @@ public class V2_Inventory {
 		return Response.ok(returnString).build();
 	}    
 
+	@Path("/{brand}/{item_number}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnSpecificBrandItem(@PathParam("brand") String brand,
+														  @PathParam("item_number") int item_number ) throws Exception{
+		
+		String returnString = null;
+		JSONArray json = new JSONArray();
+		
+		
+		try{
+		    
+		    SchemaNolbu dao = new SchemaNolbu();
+		    
+		    json = dao.queryReturnBrandItemNumber(brand,item_number);
+		    returnString = json.toString();
+		    
+		} catch(Exception e){
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}    
 }
